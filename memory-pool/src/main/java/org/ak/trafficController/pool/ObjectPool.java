b9543eeb97412835106107bcf8b881dcef577ccb
amitkhosla <amitkhosla.jobs@gmail.com>
2018-01-27 19:09:08 +0530

    Adding max items in pool option.

--- memory-pool/src/main/java/org/ak/trafficController/pool/ObjectPool.java
+++ memory-pool/src/main/java/org/ak/trafficController/pool/ObjectPool.java
@@ -1,18 +1,31 @@
 package org.ak.trafficController.pool;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 
 public class ObjectPool<T> {
+	
 	private Supplier<T> generator;
+	
+	private AtomicInteger count = new AtomicInteger();
+	
+	private int poolSize = 2000;
+	
 	public ObjectPool(Supplier<T> generator) {
 		if (generator == null) throw new RuntimeException("Generator cant be null");
 		this.generator = generator;
 	}
+	
 	private ConcurrentLinkedQueue<T> pool = new ConcurrentLinkedQueue<T>();
+	
 	public void addBackToPool(T obj) {
-		pool.add(obj);
+		if (count.get() < poolSize) {
+			count.incrementAndGet();
+			pool.add(obj);
+		} //else ignore.
 	}
+	
 	public T getFromPool() {
 		T obj = pool.poll();
 		if (obj == null) {
@@ -20,8 +33,14 @@ public class ObjectPool<T> {
 		}
 		return obj;
 	}
+	
 	public int getCount() {
 		return pool.size();
+	}
+	
+	public ObjectPool<T> setSize(int size) {
+		this.poolSize = size;
+		return this;
 	}
 	
 }
